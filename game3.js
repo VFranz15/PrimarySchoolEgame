c18a9c6c6fa28b8d139da76b5e985221822df68a
Victor Franz Domingo <92702426+VFranz15@users.noreply.github.com>
2023-11-06 00:42:39 +0800

    Add files via upload

---
+++ game3.js
@@ -0,0 +1,28 @@
+var buttonA = document.getElementById("optionA");
+var buttonB = document.getElementById("optionB");
+var buttonC = document.getElementById("optionC");
+var buttonD = document.getElementById("optionD");
+
+buttonA.addEventListener("click", getAnswer);
+buttonB.addEventListener("click", getAnswer);
+buttonC.addEventListener("click", getAnswer);
+buttonD.addEventListener("click", getAnswer);
+
+function getAnswer(event) {
+  var selectedOption = event.target; // Get the clicked button
+  var correctAnswer = "B) 4"; // Change this to the correct answer
+  if (selectedOption.textContent === correctAnswer) {
+    Swal.fire({
+        icon: 'success',
+        title: 'Correct!',
+        showConfirmButton: false,
+        timer: 1500
+      });
+  } else {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Wrong answer!'
+      })
+  }
+}
